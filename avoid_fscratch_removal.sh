77d57fd70deafb39d85576c2d9f7fc619fdd30eb
JoseCorCab <josecordoba@uma.es>
2023-05-22 14:32:40 +0200

    script fixed for large amount of files

--- avoid_fscratch_removal.sh
+++ avoid_fscratch_removal.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-files=`find $FSCRATCH| tr "\n" " "`
-touch $files
+files=`find $FSCRATCH`
+files=`echo $files | tr " " "\n"`
+limit=`echo -e "$files"| wc -l`
+processed_files=1
+while [ $processed_files -le $limit ]
+do
+	files_chunk=`echo -e "$files" | tail -n +$processed_files |head -n 1000| tr "\n" " "`
+	touch $files_chunk
+	processed_files=$(( $processed_files + 1000 ))
+done
+
