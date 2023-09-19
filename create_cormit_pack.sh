745089bc357a0b5f849625727386b9b357d0d702
JoseCorCab <josecordoba@uma.es>
2023-09-19 12:46:12 +0200

    Functional results formder changed

--- create_cormit_pack.sh
+++ create_cormit_pack.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 cormit_path=$1/coRmiT.R_0000/results
-target_fun=$1/clusters_to_enrichment.R_0000/normal_functional
+target_fun=$1/clusters_to_enrichment.R_0000/DB_functional
 output_cormit=$2/miRNA_target_results
 output_func=$2/target_functional_results
 mkdir -p $output_cormit
