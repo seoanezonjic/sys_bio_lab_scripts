b692303ac09a72e58d95547ffb1f2faa6eb85994
JoseCorCab <josecordoba@uma.es>
2023-09-19 12:16:10 +0200

    Script has been adapted for the new coRmiT workflow

--- create_cormit_pack.sh
+++ create_cormit_pack.sh
@@ -1,20 +1,12 @@
 #! /usr/bin/env bash
 
-cormit_execution_path=$1
-output=$2
-mkdir $output
-for f in $cormit_execution_path/exec*;
-do
-	exec_name=`basename $f`
-	exec_out_path=$output/$exec_name
-	functional_out_path=$exec_out_path/functional_target
-	mkdir $exec_out_path
-	cp $f/miRNA_target.html $exec_out_path/
-	cp $f/all_miRNA_summary.txt $exec_out_path/
-	cp $f/target_results_table.txt $exec_out_path/
-	mkdir $functional_out_path
-	cp $f/results/*.html $functional_out_path
-	cp $f/results/*.png $functional_out_path
-	cp $f/results/*.csv $functional_out_path
+cormit_path=$1/coRmiT.R_0000/results
+target_fun=$1/clusters_to_enrichment.R_0000/normal_functional
+output_cormit=$2/miRNA_target_results
+output_func=$2/target_functional_results
+mkdir -p $output_cormit
+mkdir -p $output_func
 
-done
+cp $cormit_path/miRNA_target.html $cormit_path/target_results_table.txt $cormit_path/hunter_results_table_translated.txt $output_cormit/
+
+cp $target_fun/*html $target_fun/*csv $output_func/
