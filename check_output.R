35ddceb85c7f7cbbdd1551d1464dd03242b875fd
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-03-01 10:16:08 +0100

    Added optparse to check_output.R script

--- check_output.R
+++ check_output.R
@@ -1,7 +1,21 @@
 #! /usr/bin/env Rscript
 
 
-check_output <- function(file, loader = readRDS, object = NULL) {
+option_list <- list(
+  optparse::make_option(c("-f", "--file"), type="character", default=NULL,
+    help="File whose existence to check."),
+  optparse::make_option(c("-l", "--loader_function"), type="character", default=readRDS,
+    help="Function with which to load the file."),
+  optparse::make_option(c("-o", "--object"), type="character", default=NULL,
+    help="Object to look for in loaded workspace. Intended if checked file is
+    		an R workspace (RData file).")
+  )
+
+opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
+
+
+check_output <- function(file = opt$file, loader = opt$loader_function,
+						 object = opt$object) {
 	if (!file.exists(file)) {
 		stop(paste0("File ", file, " not found"))
 	}
