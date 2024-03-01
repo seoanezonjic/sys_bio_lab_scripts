26669c9d64bb40fa00742ebe9b582e8a3fd3f657
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-03-01 10:35:25 +0100

    Added check for specified loader namespace. If namespace is provided, loader will be built accordingly. Function no longer has default arguments, now specified in function call, which was missing (added now that tests using it as a script have begun)

--- check_output.R
+++ check_output.R
@@ -4,7 +4,7 @@
 option_list <- list(
   optparse::make_option(c("-f", "--file"), type="character", default=NULL,
     help="File whose existence to check."),
-  optparse::make_option(c("-l", "--loader_function"), type="character", default=readRDS,
+  optparse::make_option(c("-l", "--loader_function"), type="character", default="readRDS",
     help="Function with which to load the file."),
   optparse::make_option(c("-o", "--object"), type="character", default=NULL,
     help="Object to look for in loaded workspace. Intended if checked file is
@@ -13,9 +13,18 @@ option_list <- list(
 
 opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
 
+loader_string <- opt$loader_function
+split_string <- strsplit(loader_string, split = "::")
 
-check_output <- function(file = opt$file, loader = opt$loader_function,
-						 object = opt$object) {
+if(length(split_string[[1]]) == 2) {
+	namespace <- split_string[[1]][1]
+	func <- split_string[[1]][2]
+	loader <- getFromNamespace(func, namespace)
+} else {
+	loader = match.fun(opt$loader_function)
+}
+
+check_output <- function(file, loader, object) {
 	if (!file.exists(file)) {
 		stop(paste0("File ", file, " not found"))
 	}
@@ -25,3 +34,5 @@ check_output <- function(file = opt$file, loader = opt$loader_function,
 	}
 	stop('File was loaded, but did not contain expected object')
 }
+
+check_output(file = opt$file, loader = loader, object = opt$object)
